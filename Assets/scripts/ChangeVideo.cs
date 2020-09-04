7cfb48bb0bf18d291ed899bacc3e98e26ecc612c
Emerson <emerson_10@hotmail.nl>
2020-09-04 05:55:47 +0200

    full project

---
+++ Assets/scripts/ChangeVideo.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class ChangeVideo : MonoBehaviour
+{
+
+    //SceneLoader
+    public void Changescene1()
+    {
+        SceneManager.LoadScene("Cave");
+    }
+
+    public void Changescene2()
+    {
+        SceneManager.LoadScene("Forest");
+    }
+
+    public void ChangeMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
---
+++ Assets/scripts/VideoManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class VideoManager : MonoBehaviour
+{
+    private VideoPlayer videoPLayer;
+
+    void Start()
+    {
+        videoPLayer = GetComponent<VideoPlayer>();
+    }
+
+    public void Play()
+    {
+        videoPLayer.Play();
+    }
+
+    public void Pause()
+    {
+        videoPLayer.Pause();
+    }
+
+    public void Stop()
+    {
+        videoPLayer.Stop();
+    }
+
+}
